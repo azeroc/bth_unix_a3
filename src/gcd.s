1c95bec1f3385ee05c788ca7f79ea815522f081f
Azeroc <8745995+azeroc@users.noreply.github.com>
2018-10-28 04:59:01 +0100

    Finished functions gcd and lntwo

--- src/gcd.s
+++ src/gcd.s
@@ -5,9 +5,27 @@
     .globl gcd
     .type gcd, @function
 # Arguments:
-# 1st arg, %rdi: gcd first (left) operand
-# 2nd arg, %rsi: gcd second (right) operand
+# 1st arg, %rdi: gcd first  (left)  operand X
+# 2nd arg, %rsi: gcd second (right) operand Y
 gcd:
     movq    $0, %rax    # Initialize return value
+gcd_abs_rdi:            # Make sure RDI (1st gcd operand X) is absolute value
+    cmpq    $0, %rdi
+    jge     gcd_abs_rsi
+    neg     %rdi
+gcd_abs_rsi:            # Make sure RSI (2nd gcd operand Y) is absolute value
+    cmpq    $0, %rsi
+    jge     gcd_loop
+    neg     %rsi
+gcd_loop:    
+    movq    %rdi, %rax  # %rax is dividend before division and quotient after division
+    xorq    %rdx, %rdx  # Zero out %rdx register before division 
+                        # (because dividend is combination of %rdx:%rax and we want top 64 bits / %rdx zeroed, otherwise we get floating point exception)
+    divq    %rsi        # Unsigned divide X by %rsi Y, quotient Q stored in %rax, remainder R in %rdx
+    movq    %rsi, %rdi  # X = Y
+    movq    %rdx, %rsi  # Y = R
+    cmpq    $0, %rsi    # While Y > 0, keep looping
+    jg      gcd_loop
 gcd_exit:
+    movq    %rdi, %rax  # Store result in RAX return register
     ret                 # Return with result stored in return register %rax
--- src/lntwo.s
+++ src/lntwo.s
@@ -7,6 +7,13 @@
 # Arguments:
 # 1st arg, %rdi: lntwo first operand
 lntwo:
-    movq    $0, %rax    # Initialize return value
+    movq    $0, %rsi    # Initialize result (divide-by-2 counter)
+lntwo_loop:
+    cmpq    $0, %rdi    # If RDI <= 0, jump to exit, otherwise, continue loop
+    jle     lntwo_exit
+    shrq    $1, %rdi    # rdi = rdi >> 1 (logical shift right by 1 on rdi register, same thing as rdi div by 2)
+    inc     %rsi        # result = result + 1, count how many times we managed to shift rdi before its value goes to 0
+    jmp     lntwo_loop
 lntwo_exit:
+    movq    %rsi, %rax  # Assign result to RAX return-register
     ret                 # Return with result stored in return register %rax
